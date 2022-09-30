3b98992f1735037d601c3c6b341b3ef18ef6e00c
Elizabethucar <elizg77@hotmail.com>
2022-09-30 14:06:07 +0200

    changes api

--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,10 @@ routes.get('/', userController.get_all_users)
 
 routes.get('/:id', userController.get_user)
 
-routes.put('/update/:id', userController.put_user)
+routes.put('/update:id', userController.put_user)
 
 routes.post('/post', userController.post_user)
 
-routes.delete('/delete/:id', userController.delete_user)
+routes.delete('/remove:id', userController.delete_user)
 
 module.exports = routes
